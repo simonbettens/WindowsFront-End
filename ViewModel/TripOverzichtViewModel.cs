82f9cb72e5417399a44a4822cd30821f670b1da8
Simon <simon.bettens@live.be>
2020-11-07 12:05:04 +0100

    add api call

--- ViewModel/TripOverzichtViewModel.cs
+++ ViewModel/TripOverzichtViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net.Http;
 using WindowsFront_end.Model;
+using WindowsFront_end.Util;
 
 namespace WindowsFront_end.ViewModel
 {
@@ -11,11 +15,14 @@ namespace WindowsFront_end.ViewModel
         public TripOverzichtViewModel()
         {
             TripList = new ObservableCollection<Trip>();
-            GetData();
+            //GetData();
+            GetDataAsync();
         }
 
         private void GetData()
         {
+
+
             var polen = new Trip("Polen", "#008a02", DateTime.Now, DateTime.Now.AddDays(5));
             TripList.Add(polen);
             var frankrijk = new Trip("Frankrijk", "#d8e305", DateTime.Now, DateTime.Now.AddDays(6));
@@ -27,9 +34,33 @@ namespace WindowsFront_end.ViewModel
             var singapore = new Trip("Singapore", "#ba0ba6", DateTime.Now, DateTime.Now.AddDays(5));
             TripList.Add(singapore);
         }
-
-        internal void Add()
+        private async void GetDataAsync()
         {
+            HttpClient client = new HttpClient();
+            var json = "";
+            try
+            {
+                //https://localhost:5001/api/Trip/GetAllTrips
+                json = await client.GetStringAsync(new Uri(UrlUtil.PorjectURL + "Trip/GetAllTrips"));
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            ///https://localhost:44372/api/Trip/GetAllTrips
+            var lst = JsonConvert.DeserializeObject<List<Trip>>(json);
+            foreach (Trip trip in lst)
+            {
+                this.TripList.Add(trip);
+            }
+        }
+
+        public void Add()
+        {
+
+
             var test = new Trip("test", "#81fcd9", DateTime.Now, DateTime.Now.AddDays(5));
             TripList.Add(test);
 
