7f1687442eabef57e3b17a4410168e2e2c54e051
Simon <simon.bettens@live.be>
2020-11-25 12:27:51 +0100

    add map

--- View/AddTripPage.xaml.cs
+++ View/AddTripPage.xaml.cs
@@ -49,8 +49,8 @@ namespace WindowsFront_end.View
 
         private void Save_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            AddTripViewModel.Save();
-            Frame.Navigate(typeof(TripoverzichtPage));
+            //AddTripViewModel.Save();
+            Frame.Navigate(typeof(AddRoute));
         }
 
     }
